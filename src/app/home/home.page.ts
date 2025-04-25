9c6703b31618bfa84d65024d926ce08d13da0ae6
STDano <sashadano2003@gmail.com>
2025-04-25 15:38:43 +0800

    Initial Pomodoro
    Added real time. Currently only displaying through console.log

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +8,30 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  currentTime: string = '';
+
   constructor() {}
+
+  ngOnInit() {
+    this.updateTime();
+    setInterval(() => {
+      this.updateTime();
+    }, 1000);
+  }
+
+  updateTime() {
+    const now = new Date();
+    let hours = now.getHours();
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    const hoursStr = hours.toString().padStart(2, '0');
+    this.currentTime = `${hoursStr}:${minutes}:${seconds} ${ampm}`;
+    console.log(this.currentTime);
+  }
+
+  
 
 }
